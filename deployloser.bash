8ec36036ed7f96f576e7e30ad2056fabe0ddd458
Kaushik Pahuja <kaushik.pahuja@gmail.com>
2024-10-07 16:44:19 -0700

    test deployment

--- deployloser.bash
+++ deployloser.bash
@@ -1,3 +1,1 @@
-rm -rf LoserBot 
-git clone https://github.com/Kaushik-Pahuja-Portfolio/LoserBot
-pip install -r LoserBot/requirements.txt && nohup python3 LoserBot/LoserBot.py & exit
+pip install -r requirements.txt && nohup python3 LoserBot.py & exit
