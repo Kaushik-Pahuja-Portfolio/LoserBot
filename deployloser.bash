c8a5312804946d9e59f2fe8ab717fe16886f9f13
Kaushik Pahuja <kaushik.pahuja@gmail.com>
2024-10-08 00:25:19 -0700

    Update deployloser.bash
    make it write pid, we'll worry about killing it later

--- deployloser.bash
+++ deployloser.bash
@@ -1,1 +1,1 @@
-pip install -r LoserBot/requirements.txt && nohup python3 LoserBot/LoserBot.py > loserbot.log 2> loserbot.err < /dev/null & exit
+pip install -r LoserBot/requirements.txt && nohup python3 LoserBot/LoserBot.py > loserbot.log 2> loserbot.err < /dev/null & echo $! > run.pid && exit
