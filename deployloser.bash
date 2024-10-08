b886d9d32ab21ebbbe5faf6094418e9b45d2dee0
Kaushik Pahuja <kaushik.pahuja@gmail.com>
2024-10-08 00:21:20 -0700

    Update deployloser.bash
    maybe it's in deployloser?

--- deployloser.bash
+++ deployloser.bash
@@ -1,1 +1,1 @@
-pip install -r LoserBot/requirements.txt && kill -9 `cat run.pid` && nohup python3 LoserBot/LoserBot.py > /dev/null 2>&1 & echo $! > run.pid
+pip install -r LoserBot/requirements.txt && nohup python3 LoserBot/LoserBot.py > loserbot.log 2> loserbot.err < /dev/null & exit
