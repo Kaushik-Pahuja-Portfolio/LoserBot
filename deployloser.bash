075134fbcaad330d59f0516665b983182098dd81
Kaushik Pahuja <kaushik.pahuja@gmail.com>
2024-10-08 00:28:32 -0700

    Update deployloser.bash
    this should pkill only if run.pid exists

--- deployloser.bash
+++ deployloser.bash
@@ -1,1 +1,1 @@
-pip install -r LoserBot/requirements.txt && nohup python3 LoserBot/LoserBot.py > loserbot.log 2> loserbot.err < /dev/null & echo $! > run.pid && exit
+pip install -r LoserBot/requirements.txt && if [-f run.pid]; then kill -9 `cat run.pid` fi && nohup python3 LoserBot/LoserBot.py > loserbot.log 2> loserbot.err < /dev/null & echo $! > run.pid && exit
